1fc826566f3ed55df17f4fd100ca23a444deb7bb
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2024-02-12 21:57:19 +0600

    Main section is divided into different sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import Header from "./Components/Header/Header";
+import WeatherBoard from "./Components/Weather/WeatherBoard";
 
 function App() {
   return (
     <>
       <Header />
+      <main>
+        <section className="">
+          <WeatherBoard />
+        </section>
+      </main>
     </>
   );
 }
