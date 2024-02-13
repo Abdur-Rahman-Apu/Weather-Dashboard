7d2b6e01fda81ef589331e0218e66dd247c7bf0e
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2024-02-13 19:00:52 +0600

    Favourite list show completed

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,11 @@
+import { useState } from "react";
 import Favourite from "./Favourite";
 import FavouriteList from "./FavouriteList";
 import Logo from "./Logo";
 import Search from "./Search";
 
 export default function Header() {
+  const [show, setShow] = useState(false);
   return (
     <header className="fixed w-full top-0 z-50 bg-gradient-to-b from-black/60 to-black/0 pb-10">
       <nav className="container flex items-center justify-between pt-5">
@@ -11,8 +13,8 @@ export default function Header() {
 
         <div className="flex items-center gap-4 relative">
           <Search />
-          <Favourite />
-          <FavouriteList />
+          <Favourite onShow={() => setShow(!show)} />
+          {show && <FavouriteList />}
         </div>
       </nav>
     </header>
